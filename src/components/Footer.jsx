0d3a29a5eef37f83a58595a209bd21fdf34701ab
piyush7833 <96736508+piyush7833@users.noreply.github.com>
2023-09-27 11:43:29 +0530

    sec

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import FooterImage from '../../public/images/f1.png'
+const Footer = () => {
+  return (
+    <div className={`w-[98%] flex`}>
+    </div>
+  )
+}
+
+export default Footer
