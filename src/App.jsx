1e49bb8785107f312b85df4e5be2e73a9103a84b
piyush7833 <96736508+piyush7833@users.noreply.github.com>
2023-09-27 12:32:20 +0530

    ps1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import DeepakComponent1 from './components/DeepakComponent1'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    <div >
+      <DeepakComponent1/>
+      <Footer/>
+    </div>
   )
 }
 
